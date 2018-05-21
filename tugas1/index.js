0d1f6598de8f7204d3161e276e83032ed8564306
SenimanMalam001 <elfinsanjaya12@gmail.com>
2018-05-22 01:44:05 +0700

    tugas 1

---
+++ tugas1/index.js
@@ -0,0 +1,12 @@
+let hrglass = generateHourglass = () => {
+
+  };
+
+let printHourglass = () => {
+
+  };
+console.log(typeof hrglass);
+console.log(printHourglass(hrglass));
+
+let hrglass1 = generateHourglass(1);
+console.log(hrglass1);
